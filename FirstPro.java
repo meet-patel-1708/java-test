88a76bf9cd6c7fbd096036795f27b41d2ea80dde
meet-patel-1708 <meetpatel692020@gmail.com>
2025-10-16 14:29:26 +0530

    updated FirstPro.java

--- FirstPro.java
+++ FirstPro.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 public class FirstPro {
     public static void main(String[] args) {
-        System.out.println("This is new java program changes");
+        System.out.println("Hello Meet! This is new java program changes");
         System.out.println(new Date());
     }
 }
