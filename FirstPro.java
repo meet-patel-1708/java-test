7f9897e3a843eace4129985b78b34cb4a0a955fa
meet-patel-1708 <meetpatel692020@gmail.com>
2025-10-16 12:25:08 +0530

    first commit

---
+++ FirstPro.java
@@ -0,0 +1,10 @@
+package com.collections;
+
+import java.util.Date;
+
+public class FirstPro {
+    public static void main(String[] args) {
+        System.out.println("This is java program");
+        System.out.println(new Date());
+    }
+}
